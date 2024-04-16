65d4ff8e4dbf96de8efe88971c3d86abebed6506
Qu-Ack <134773611+Qu-Ack@users.noreply.github.com>
2024-04-17 02:00:50 +0530

    clerk sign in

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -4,6 +4,9 @@ import Github from "@/public/github.svg"
 import Link from "next/link"
 import Instagram from "@/public/instagram.svg"
 import Linkedin from "@/public/linkedin.svg"
+import { SignedIn, UserButton } from "@clerk/nextjs"
+import { SignedOut } from "@clerk/nextjs"
+ 
 
 export default function NavBar() {
     return (
@@ -11,12 +14,17 @@ export default function NavBar() {
             <div className="flex justify-between w-screen bg-lime-900 h-[7vh] text-white font-sans text-sm pr-[256px] pl-[256px] items-center">
                 <p>New Delhi, India</p>
                 <div className="flex gap-[2rem]">
-                    <Link href="/sign-in" >Sign In</Link>
-                    <Link href="/sign-up">Sign Up</Link>
+                    <SignedOut>
+                        <Link href="/sign-in" >Sign In</Link>
+                        <Link href="/sign-up">Sign Up</Link>
+                    </SignedOut>
+                    <SignedIn>
+                        <UserButton/>
+                    </SignedIn>
                 </div>
 
             </div>
-            <nav className="flex ml-[12rem] w-[80vw] justify-around pl-[256px] pr-[256px] pt-8 pb-8">
+            <nav className="flex ml-[12rem] w-[80vw] justify-around pl-[256px] pr-[256px] pt-8 pb-8 border-black-300 border-b-3">
                 <div className="flex">
                     <ul className="flex gap-[5rem] list-none items-center">
                         <li>
@@ -43,7 +51,7 @@ export default function NavBar() {
 
             </nav>
 
-            <div className="h-[20vh] w-screen bg-orange-300 flex flex-col gap-[0.5rem] justify-center">
+            {/* <div className="h-[20vh] w-screen bg-orange-300 flex flex-col gap-[0.5rem] justify-center">
                 <Link className="flex justify-center items-center gap-[1rem] text-white font-sans" href="https://github.com/Qu-Ack">
                     Follow Me on My Github
                     <Image src={Github} alt="github" width={20} height={20} />
@@ -56,7 +64,7 @@ export default function NavBar() {
                     Connect with me on Linkedin
                     <Image src={Linkedin} alt="github" width={20} height={20} />
                 </Link>
-            </div>
+            </div> */}
         </>
     )
 }
